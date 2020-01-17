e9af1a54ba0500c71e87872467bad01a348ce86c
c-concio <chris-768@hotmail.com>
2020-01-16 21:43:58 -0500

    Added The buttons for the Admin Main Page

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from View.Admin import screens
 import kivy
-kivy.require('1.0.6') # replace with your current kivy version !
+kivy.require('1.11.1')  # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.core.window import Window
 
+# make the app fullscreen
+Window.fullscreen = 'auto'
 
-class MyApp(App):
+# use the kv definitions found in the adminScreens.kv file
+Builder.load_file('View/Admin/adminScreens.kv')
+
+# initialize Screen manager
+screenManager = ScreenManager()
+screenManager.add_widget(screens.AdminMainScreen(name='Admin Page'))
+
+class MainApp(App):
 
     def build(self):
-        return Label(text='Hello world')
+        return screenManager
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    MainApp().run()
